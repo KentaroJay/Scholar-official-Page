6b90d46cbb1f25338ea4ec4619ff2edc98d8d216
MacBook Pro <macbookpro@MacBook-no-MacBook-Pro.local>
2020-06-06 12:18:11 +0900

    minor changes to header link

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -13,7 +13,7 @@ $(window).scroll(function(){
 $(function() {
 // ボタンをクリックしたら発動
   $('.area1 button').click(function() {
-   
+
     // class'showUp'をdiv要素に追加
     $('.comment1-2').slideToggle();
 
@@ -23,7 +23,7 @@ $(function() {
 $(function() {
     // ボタンをクリックしたら発動
       $('.area2 button').click(function() {
-       
+
         // class'showUp'をdiv要素に追加
         $('.comment2-2').slideToggle();
  });
@@ -32,8 +32,41 @@ $(function() {
 $(function() {
     // ボタンをクリックしたら発動
       $('.area3 button').click(function() {
-       
+
         // class'showUp'をdiv要素に追加
         $('.comment3-2').slideToggle();
  });
+ $(document).ready(function(){
+  //URLのハッシュ値を取得
+  var urlHash = location.hash;
+  //ハッシュ値があればページ内スクロール
+  if(urlHash) {
+    //スクロールを0に戻す
+    $('body,html').stop().scrollTop(0);
+    setTimeout(function () {
+      //ロード時の処理を待ち、時間差でスクロール実行
+      scrollToAnker(urlHash) ;
+    }, 100);
+  }
+
+  //通常のクリック時
+  $('a[href^="#"]').click(function() {
+    //ページ内リンク先を取得
+    var href= $(this).attr("href");
+    //リンク先が#か空だったらhtmlに
+    var hash = href == "#" || href == "" ? 'html' : href;
+    //スクロール実行
+    scrollToAnker(hash);
+    //リンク無効化
+    return false;
+  });
+
+  // 関数：スムーススクロール
+  // 指定したアンカー(#ID)へアニメーションでスクロール
+  function scrollToAnker(hash) {
+    var target = $(hash);
+    var position = target.offset().top;
+    $('body,html').stop().animate({scrollTop:position}, 500);
+  }
+})
 });
