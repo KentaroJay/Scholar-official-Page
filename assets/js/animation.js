3c2fcda57bd7586e3f0e3ab5db7196bb01a532e9
MacBook Pro <macbookpro@MacBook-no-MacBook-Pro.local>
2020-06-06 13:34:55 +0900

    minor changes

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -54,6 +54,19 @@ $(function() {
     //リンク無効化
     return false;
   });
+  /* $('a[href^="#"').click(function() {
+    // スクロールの速度
+    var speed =  500; // ミリ秒
+    // アンカーの値取得
+    var href= $(this).attr("href");
+    // 移動先を取得
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    // 移動先を数値で取得
+    var position = target.offset().top;
+    // スムーススクロール
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  }) */
 
   // 関数：スムーススクロール
   // 指定したアンカー(#ID)へアニメーションでスクロール
